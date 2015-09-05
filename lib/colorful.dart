a7c785afbacaaa19da3c02bf97f6ab914cf60a30
mswift42 <martin.haesler@gmail.com>
2015-09-05 14:52:42 +0100

    Add isValid function.

--- lib/colorful.dart
+++ lib/colorful.dart
@@ -14,4 +14,10 @@ class Color {
   get r => _r;
   get g => _g;
   get b => _b;
+
+  bool isValid() {
+    return 0 <= this._r && this._r <= 255 &&
+           0 <= this._g && this._g <= 255 &&
+           0 <= this._b && this._b <= 255;
+  }
 }
