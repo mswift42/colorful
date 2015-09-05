7928bf6eaef552b8ecd8d7ed111413683d37526d
mswift42 <martin.haesler@gmail.com>
2015-09-05 20:55:46 +0100

    set color instance fields to final.

--- lib/colorful.dart
+++ lib/colorful.dart
@@ -5,9 +5,9 @@
 library colorful;
 
 class Color {
-  double _r;
-  double _g;
-  double _b;
+  final double _r;
+  final double _g;
+  final double _b;
 
   Color(this._r, this._g, this._b);
 
@@ -20,6 +20,7 @@ class Color {
            0.0 <= this._g && this._g <= 1.0 &&
            0.0 <= this._b && this._b <= 1.0;
   }
+  
 
   
   
