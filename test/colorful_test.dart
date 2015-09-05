6d506d495e8e45d8cb95f7ce2078a54c0009b5b0
mswift42 <martin.haesler@gmail.com>
2015-09-05 19:37:34 +0100

    Fix tests to use double instead of int.

--- test/colorful_test.dart
+++ test/colorful_test.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// Copyright (c) 2015, <Martin Haesler>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 library colorful.test;
@@ -8,29 +8,29 @@ import 'package:test/test.dart';
 
 void main() {
   test('instance fields of class Color', () {
-    Color c1 = new Color(22,33,44);
-    expect(c1.r, 22);
-    expect(c1.b, 44);
-    expect(c1.g, 33);
+    Color c1 = new Color(0.1,0.1,0.8);
+    expect(c1.r, 0.1);
+    expect(c1.b, 0.8);
+    expect(c1.g, 0.1);
   });
   test('isValid returns true for acceptable rgb values', () {
-    Color c1 = new Color(0,0,0);
+    Color c1 = new Color(0.0,0.0,0.0);
     expect(c1.isValid(), true);
-    Color c2 = new Color(255,255,255);
+    Color c2 = new Color(1.0,1.0,1.0);
     expect(c2.isValid(), true);
-    Color c3 = new Color(0,0,255);
+    Color c3 = new Color(0.0,0.0,1.0);
     expect(c2.isValid(), true);
   });
   test('isValid returns false for out of bounds rgb values', () {
-    Color c1 = new Color(0,0,-1);
+    Color c1 = new Color(0.0,0.0,-0.1);
     expect(c1.isValid(), false);
-    Color c2 = new Color(255,255,256);
+    Color c2 = new Color(1.0,1.0,1.1);
     expect(c2.isValid(), false);
-    Color c3 = new Color(500,500,500);
+    Color c3 = new Color(2.0,2.0,2.0);
     expect(c3.isValid(), false);
-    Color c4 = new Color(256,255,255);
+    Color c4 = new Color(1.1,1.0,1.0);
     expect(c4.isValid(), false);
-    Color c5 = new Color(0,256,0);
+    Color c5 = new Color(0.0,1.1,0.0);
     expect(c5.isValid(), false);
   });
 }
