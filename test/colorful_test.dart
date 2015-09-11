13735f8736f87f3f36064ab0f279b1a32c0221c4
mswift42 <martin.haesler@gmail.com>
2015-09-11 15:57:28 +0100

    Fix initialisation tests

--- test/colorful_test.dart
+++ test/colorful_test.dart
@@ -8,28 +8,28 @@ import 'package:test/test.dart';
 
 void main() {
   test('initialize ColorPalette', () {
-    ColorPalette cp = new ColorPalette(3);
+    ColorPalette cp = new ColorPalette(3, 20.0,20.0);
     expect(cp.size, 3);
-    expect(cp.colors.length, 0);
+    expect(cp.colors.length, 3);
   });
   test('randomHue returns valuse between 0 and 359', () {
-    var cp = new ColorPalette(3);
+    var cp = new ColorPalette(3,20.0,20.0);
     for (int i = 0;i<10000;i++) {
       var a = cp.randomHue();
       expect(a >= 0.0, true);
       expect(a <= 359.0, true);
     }
   });
-  test('hueRande returns a List of length size.', () {
-    var cp = new ColorPalette(3);
+  test('hueRange returns a List of length size.', () {
+    var cp = new ColorPalette(3, 20.0, 20.0);
     var hr = cp.hueRange(20.0);
     expect(hr.length, 3);
   });
   test('hueRange values are all below 360', () {
-    var cp = new ColorPalette(8);
+    var cp = new ColorPalette(8, 20.0,20.0);
     var hr = cp.hueRange(20.0);
     expect(hr.any((i) => (i > 359.0)), false);
-    cp = new ColorPalette(10);
+    cp = new ColorPalette(10, 20.0, 20.0);
     hr = cp.hueRange(300.0);
     expect(hr.any((i) => (i > 359.0)), false);
   });
