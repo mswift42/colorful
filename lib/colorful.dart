c9239384fbe09367917c5438b34d69d903209697
mswift42 <martin.haesler@gmail.com>
2015-09-13 12:14:34 +0100

    Add link to color package.

--- lib/colorful.dart
+++ lib/colorful.dart
@@ -3,7 +3,10 @@
 
 /// The colorful library.
 /// Create a List of equally distant random colors.
-/// Each color in the color list has the same values
+/// Each color in the color list is generated
+/// from the HSL color model using the
+/// [Color](https://pub.dartlang.org/packages/color) package,
+/// and shares the same values
 /// for saturation and lightness.
 ///
 /// ColorPalettes can be generated from predefined palettes,
